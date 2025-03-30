795d921d75d69fff642eaeeec8d9a05922a09acb
drakhz <naiitder@gmail.com>
2025-03-31 00:04:27 +0200

    LoadingScene

---
+++ SwampKing/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
+    private float targetProgress;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        _loaderCanvas.SetActive(false);
+    }
+
+
+    public async void LoadScene(string sceneName)
+    {
+        AsyncOperation scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+            targetProgress = scene.progress;
+        } while (scene.progress < 0.9f);
+
+        await Task.Delay(1000);
+
+        scene.allowSceneActivation = true;
+    }
+
+    private void Update()
+    {
+        _progressBar.fillAmount = Mathf.MoveTowards(_progressBar.fillAmount, targetProgress, 3*Time.deltaTime);
+    }
+}
