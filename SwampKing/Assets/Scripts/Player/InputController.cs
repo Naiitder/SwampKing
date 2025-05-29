550cc544939268877b5b192d75de0cbe7e97e5a8
Naiitder <naiitder@gmail.com>
2025-05-29 16:06:54 +0200

    Improving Combat part 01

--- SwampKing/Assets/Scripts/Player/InputController.cs
+++ SwampKing/Assets/Scripts/Player/InputController.cs
@@ -219,7 +219,7 @@ public class InputController : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1.5f);
 
             if (InputBuffer.Count > 0)
             {
